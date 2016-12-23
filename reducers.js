0b463c37f683eb15ebaa3e6d2e4ba5c763dc29e6
hathbanger <hathbanger.ah@gmail.com>
2016-12-22 19:18:04 -0700

    cleaning

--- reducers.js
+++ reducers.js
@@ -4,17 +4,6 @@ import {
   TOGGLE_TASK_SUCCESS, TOGGLE_TASK_ATTEMPT
 } from './actions'
 
-const initialState = {
-     tasks: [{"item":"TV", "phone":"1234561234", "done": false}, 
-     {"item":"Phone", "phone":"1234561234", "done": true}, 
-     {"item":"Internet", "phone":"1234561234", "done": false}, 
-     {"item":"Best Bundle", "phone":"1234561234", "done": true}, 
-     {"item":"GIVE AWAY", "phone":"1234561234", "done": true}, 
-     {"item":"Security", "phone":"1234561234", "done": true}, 
-     {"item":"Gas", "phone":"1234561234", "done": true}, 
-     {"item":"Electricity", "phone":"1234561234", "done": true}]
-   }
-
 const setLocalStorage = (state) => {localStorage.setItem("cyhList", JSON.stringify(state)); return state}
 const fetchLocalStorage = (key) => {return JSON.parse(localStorage.getItem(key))}
 
