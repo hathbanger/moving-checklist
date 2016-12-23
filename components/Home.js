304268866fdce17b30e5876c7d1b3ad5a3decdc2
hathbanger <hathbanger.ah@gmail.com>
2016-12-23 13:55:09 -0700

    cleaning

--- components/Home.js
+++ components/Home.js
@@ -4,25 +4,17 @@ import { ProgressBar } from 'react-bootstrap';
 
 
 export default class Home extends Component {
-isDone(value) {
-  return value.done == true;
-}
-
+  isDone(value) {
+    return value.done == true;
+  }
   render() {
-
     const { dispatch, errorMessage, tasks } = this.props
     const progress = (this.props.tasks.filter(this.isDone).length / this.props.tasks.length) * 100
-    console.log("progress",progress)
     return (
       <div className="container">
         <div className="jumbotron">
-        <ProgressBar now={progress} />
-          <ul className="list-group">
-            <div className="form-group">            
-                <CheckListContainer checkBoxes={this.props.tasks} dispatch={dispatch} />
-            </div>    
-          </ul>  
-           
+          <ProgressBar now={progress} />         
+          <CheckListContainer checkBoxes={this.props.tasks} dispatch={dispatch} />
         </div>       
       </div>
     )
