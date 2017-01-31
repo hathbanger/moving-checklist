49889f7c42b3b4af5dc6a29249151ce0275f6413
hathbanger <hathbanger.ah@gmail.com>
2017-01-31 12:05:20 -0700

    components => stateless

--- components/ServiceCell.js
+++ components/ServiceCell.js
@@ -1,13 +1,8 @@
-import React, { Component, PropTypes } from 'react'
-export default class ServiceCell extends Component {
-  render() {
-  	const { checkBox } = this.props
-    return (
+import React from 'react'
+
+const ServiceCell = ({checkBox}) =>
 		<td colSpan="1">
 		  <h6 className="center-block text-left">{checkBox.item}</h6>
 		</td>
-    )
-  }
-}
 
-
+export default ServiceCell
