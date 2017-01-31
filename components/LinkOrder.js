af40089b6ea9ed47c8efd25ecbc3bde3fc97fc64
hathbanger <hathbanger.ah@gmail.com>
2017-01-31 12:58:15 -0700

    cleaned up commented out functions for es6, BABY. es6, y u so bb

--- components/LinkOrder.js
+++ components/LinkOrder.js
@@ -12,26 +12,3 @@ const LinkOrder = ({link, toggleTask}) =>
 	        </a>
 
 export default LinkOrder
-
-
-// export default class LinkOrder extends Component {
-//   render() {
-//   	const { link } = this.props
-//     return (
-// 	        <a 
-// 	          href={`${link}`} 
-// 	          target="_blank" 
-// 	          onClick={(event) => this.handleToggle()}>
-// 		          <h6>
-// 		            Order Now
-// 		          </h6>
-// 	        </a>	        
-//     )
-//   }
-
-//   handleClick(){
-//   	this.props.toggleTask();
-//   }
-// }
-
-
